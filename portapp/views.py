64e21675946853028885d80b1e190d26dca144a4
dicksonkariuki <dicksonkariuki4@gmail.com>
2020-01-07 10:34:52 +0300

    Add index view

--- portapp/views.py
+++ portapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from portapp.models import Project
 
 # Create your views here.
+def project_index (request):
+    projects=Project.objects.all()
+    context={
+        'projects':projects
+    }
+    return render (request,'project_index.html',context)
